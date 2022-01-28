799e567c3f3ffb4d769165497ad77f2936ac2d67
Pulie <2009048@upy.edu.mx>
2022-01-27 21:44:16 -0600

    Scan and print two numbers

---
+++ Unit 1/inputs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int num1;
+int num2;
+int main(){
+
+    printf("Enter the first number! \n\tnum1:");
+    scanf("%d", &num1);
+    printf("Enter the second number! \n\tnum2:");
+    scanf("%d",&num2);
+
+
+
+
+    return 0;
+}
