4a463432640dec75817f46ed49ff4ede6526d010
Pulie <2009048@upy.edu.mx>
2022-02-11 11:29:50 -0600

    Added another way to solve the code

--- Unit 1/even numbers.c
+++ Unit 1/even numbers.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
-int main(){
+#include <stdlib.h>
+#include <stdbool.h>
+/*int main(){
     int number;
     printf("Welcome! Enter an integer number to check if it is even or odd\n");
     scanf("%d",&number);
@@ -10,3 +11,15 @@ int main(){
         printf("You have entered the number %d. This is an odd number!",number);
     }
 }
+*/
+    int main(int argc, char** argv){
+        int numberToTest = atoi(argv[1]);
+        bool isDividedByTwO = (numberToTest %2 == 0);
+
+        if (isDividedByTwO == true) {
+            printf("You have entered the number %d. This is an even number!",numberToTest);
+
+        } else{
+            printf("You have entered the number %d. This is an odd number!",numberToTest);
+        }
+    }
