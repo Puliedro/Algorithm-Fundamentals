f4adc632b6cd4bd14c54347ab8095d36d5f9b2cf
Puliedro <2009048@upy.edu.mx>
2022-04-08 14:26:35 -0500

    Pushed local files for portfolio

---
+++ unit2/nestedfor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char * myName = "Héctor González"; 
+
+
+int main (int argc, char ** argv){
+    printf("Welcome to program %s\n", argv[0]);
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    int sum = num1 + num2;
+    printf("The sum is equal to %d\n", sum);
+
+    return 0;
+}
---
+++ unit2/pointers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//variables
+int x = 0;
+int* p = &x;
+int num ;
+
+
+// modify variable's values with pointers
+
+void modifyVariable(int* pointer){
+    for (int i = 1; i <= num; i++){
+        if ( num % i ==0){
+            *p = x + 1;
+        }
+    }
+    return;
+}
+
+int main(){
+    printf("Type your number: \n");
+    scanf("%d", &num);
+    //call the modifyVariable funciton
+    modifyVariable(p);
+    if (x == 2){
+        printf("The number %d is prime\n", num);
+    }
+    else {
+        printf("The number %d is not prime\n", num);
+    }
+
+}
+
